80d315660ace8ac05ba792ef6d7158f034992e12
Lemon丶Roc <1411580233@qq.com>
2018-11-18 16:41:22 +0800

    提交文件到Git

---
+++ git-test/src/com/itheima/test/Test.java
@@ -0,0 +1,18 @@
+package com.itheima.test;
+
+/**
+ * @Author: Qin Pengcheng
+ * @CreateDate: 2018/11/18 16:26
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("sdvasodiad");
+        System.out.println("sdvasodiad");
+        System.out.println("sdvasodiad");
+        System.out.println("sdvasodiad");
+        System.out.println("sdvasodiad");
+        System.out.println("sdvasodiad");
+        System.out.println("sdvasodiad");
+    }
+}
